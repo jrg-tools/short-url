23bafb585715e86ef78921891b798be2ea4ed7e7
jorgechato <hack@jorgechato.com>
2024-11-29 15:12:33 +0900

    feat(routes): implement all endpoints

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,23 +1,43 @@
+import type { Bindings } from '@/env.d';
+import { createShortUrl, deleteShortUrl, searchShortUrl } from '@/repository/actions';
+import { InternalServerError, NotFound } from '@/routes/errors';
 import { aliasSchema, originUrlSchema, querySchema } from '@/utils/validator';
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 
-const admin = new Hono()
+const admin = new Hono<{ Bindings: Bindings }>()
 
   .get('/search', zValidator('query', querySchema), async (c) => {
-    const query = c.req.query('q');
-    return c.json({ listOfAliasMatching: query });
+    const query: string = c.req.query('q')!;
+
+    const { error, list } = await searchShortUrl(c, query);
+    if (error) {
+      return c.json({ message: NotFound }, 400);
+    }
+
+    return c.json(list);
   })
 
   .post('/', zValidator('json', originUrlSchema), async (c) => {
     const body = await c.req.json();
 
-    return c.json({ createShortUrlFor: body });
+    const { error, res } = await createShortUrl(c, body.originUrl);
+    if (error) {
+      return c.json({ message: InternalServerError }, 500);
+    }
+
+    return c.json(res);
   })
 
   .delete('/:id', zValidator('param', aliasSchema), async (c) => {
-    const id = c.req.param('id');
-    return c.json({ deleteAlias: id });
+    const id: string = c.req.param('id');
+
+    const { error } = await deleteShortUrl(c, id);
+    if (error) {
+      return c.json({ message: NotFound }, 400);
+    }
+
+    return c.status(204);
   });
 
 export default admin;
--- src/routes/shortUrl.ts
+++ src/routes/shortUrl.ts
@@ -1,12 +1,21 @@
+import type { Bindings } from '@/env.d';
+import { getOriginUrlByAlias } from '@/repository/actions';
+import { NotFound } from '@/routes/errors';
 import { aliasSchema } from '@/utils/validator';
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 
-const open = new Hono()
+const open = new Hono<{ Bindings: Bindings }>()
 
   .get('/:id', zValidator('param', aliasSchema), async (c) => {
-    const id = c.req.param('id');
-    return c.json({ redirectTo: id });
+    const id: string = c.req.param('id');
+
+    const { error, origin } = await getOriginUrlByAlias(c, id);
+    if (error || !origin || origin === '') {
+      return c.json({ message: NotFound }, 400);
+    }
+
+    return c.redirect(origin);
   });
 
 export default open;
