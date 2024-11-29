13a250b071c0c9da3785ab5dc2b7a1afc67bf5be
jorgechato <hack@jorgechato.com>
2024-11-29 15:56:02 +0900

    feat(routes): increase hits before redirect

--- src/routes/shortUrl.ts
+++ src/routes/shortUrl.ts
@@ -1,5 +1,5 @@
 import type { Bindings } from '@/env.d';
-import { getOriginUrlByAlias } from '@/repository/actions';
+import { getOriginUrlByAlias, increaseHits } from '@/repository/actions';
 import { NotFound } from '@/routes/errors';
 import { aliasSchema } from '@/utils/validator';
 import { zValidator } from '@hono/zod-validator';
@@ -10,12 +10,17 @@ const open = new Hono<{ Bindings: Bindings }>()
   .get('/:id', zValidator('param', aliasSchema), async (c) => {
     const id: string = c.req.param('id');
 
-    const { error, origin } = await getOriginUrlByAlias(c, id);
-    if (error || !origin || origin === '') {
+    const { error, res } = await getOriginUrlByAlias(c, id);
+    if (error || !res) {
       return c.json({ message: NotFound }, 400);
     }
 
-    return c.redirect(origin);
+    const { error: err } = await increaseHits(c, res.Alias, res.Hits);
+    if (err) {
+      return c.json({ message: NotFound }, 400);
+    }
+
+    return c.redirect(res.Origin);
   });
 
 export default open;
