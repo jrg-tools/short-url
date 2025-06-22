724e72c885608e7653045cfc05330bed818ce650
jorgechato <hack@jorgechato.com>
2025-06-22 22:45:20 +0900

    change(/): remove ff for now

--- src/routes/shortUrl.ts
+++ src/routes/shortUrl.ts
@@ -8,19 +8,19 @@ import { getOriginUrlByAlias, increaseHits } from '@/repository/actions';
 
 const open = new Hono<{ Bindings: Bindings }>()
   .use('*', clerkMiddleware())
-  .use('*', createFeatureFlagMiddleware())
+  // .use('*', createFeatureFlagMiddleware())
 
   .get('/:id', zValidator('param', aliasSchema), async (c) => {
     const id: string = c.req.param('id');
 
     const res = await getOriginUrlByAlias(c, id);
 
-    const tracking_hits = await c.var.getFeatureFlag('shorturl_track_hits', {
-      defaultValue: false,
-    });
-    if (tracking_hits) {
-      await increaseHits(c, res.Alias, res.Hits);
-    }
+    // const tracking_hits = await c.var.getFeatureFlag('shorturl_track_hits', {
+    // defaultValue: false,
+    // });
+    // if (tracking_hits) {
+    // await increaseHits(c, res.Alias, res.Hits);
+    // }
 
     return c.redirect(res.Origin);
   });
